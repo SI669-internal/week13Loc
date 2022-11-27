113a870e6508d0246adc556b25256d113bfc9284
Mark Newman <mwnewman@umich.edu>
2022-11-27 18:29:39 -0500

    basic loc and maps

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+import { 
+  requestForegroundPermissionsAsync,
+  watchPositionAsync 
+} from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [ permissionsGranted, setPermissionsGranted ] = useState(false);
+
+  let unsubscribeFromLocation = null;
+
+  const subscribeToLocation = async () => {
+
+    let { status } = await requestForegroundPermissionsAsync();
+    setPermissionsGranted(status === 'granted');
+
+    if (unsubscribeFromLocation) {
+      unsubscribeFromLocation();
+    }
+
+    unsubscribeFromLocation = watchPositionAsync({}, location => {
+      console.log('received update:', location);
+      setLocation(location);
+    })
+  }
+
+  useEffect(() => {
+    subscribeToLocation();
+  }, []);
+
+  console.log(PROVIDER_GOOGLE);
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>
+        {permissionsGranted ?
+          location ?
+            `lat: ${location.coords.latitude} \n` + 
+            `lon: ${location.coords.longitude}`
+          :
+            "Waiting..."
+        :
+          "Location permission granted."
+        }
+
+      </Text>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE}/> 
     </View>
   );
 }
@@ -13,8 +56,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  paragraph: {
+    fontSize: 24
+  },
+  map: {
+    flex: 0.5,
+    width: '100%',
+    height: '100%'
+  }
+})
